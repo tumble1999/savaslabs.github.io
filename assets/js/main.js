316cd60097c8195c7ca298b6604358214eb60d4c
Kosta Harlan <kosta@kostaharlan.net>
2015-03-30 21:24:42 -0400

    Initial commit (take 2)

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+---
+library: jquery-2.1.3.min.js
+---
+
+{% include_relative _lib/{{page.library}} %}
+
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
