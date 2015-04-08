3668b14c6bc8203b2aa628d95e3d278fe9b8fe73
Kosta Harlan <kosta@kostaharlan.net>
2015-04-08 10:40:37 -0400

    Ignore drupal.org links for now.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 bundle exec jekyll build
-bundle exec htmlproof ./_site
+bundle exec htmlproof ./_site --verbose --href-ignore "/^(?:http(?:s)?:\/\/)?(?:[^\.]+\.)?drupal\.org(?:/.*)?$/"
