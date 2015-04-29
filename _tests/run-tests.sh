7073aa7ab88bd44e8d5eb0cf87175b12e8206821
Kosta Harlan <kosta@kostaharlan.net>
2015-04-29 12:52:14 -0400

    Tests: only fail on HTTP 4xx errors

--- _tests/run-tests.sh
+++ _tests/run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 bundle exec jekyll build --config _config.yml,_config.test.yml
-bundle exec htmlproof ./_site --verbose
+bundle exec htmlproof ./_site --verbose --only-4xx
