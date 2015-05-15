c121cc3e66ca07a3165d3a54d202145a96c36c06
Anne Tomasevich <annetvich@gmail.com>
2015-05-15 14:30:42 -0400

    Feature #43: Output comments on posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,37 @@ $(document).ready(function() {
     });
   });
 
+  // Expandable comment section.
   var expanderTrigger = document.getElementById("js-expander-trigger");
   var expanderContent = document.getElementById("js-expander-content");
 
   $('#js-expander-trigger').click(function(){
     $(this).toggleClass("expander-hidden");
+
+    // Create variable for request URI.
+    var commentServer = "{{ site.comment_server_url }}";
+    var postSlug = window.location.pathname;
+    // Remove leading forward slash.
+    var truncatedSlug = postSlug.substring(1, postSlug.length)
+    var encodedSlug = encodeURIComponent(truncatedSlug);
+    var requri = commentServer+'/api/comments/post/'+encodedSlug;
+
+    $.getJSON(requri, function(json) {
+      var outhtml = '';
+      // Loop through comments.
+      $.each( json.data, function( i, val ) {
+
+        // Assign JSON data points to variables.
+        var name = json.data[i].name;
+        var created = json.data[i].created_at;
+        var comment = json.data[i].comment;
+
+        // Create HTML output.
+        outhtml = outhtml + '<div class="comment"><h5>'+name+'</h5>';
+        outhtml = outhtml + '<p>'+created+'</p>';
+        outhtml = outhtml + '<p>'+comment+'</p></div>';
+      });
+      $('#post-comments').html(outhtml);
+    });
   });
 });
