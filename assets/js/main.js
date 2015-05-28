38c9815e65f2ae94e62a91e2a738f6d5b42a310a
Kosta Harlan <kosta@kostaharlan.net>
2015-05-28 14:10:57 -0400

    More work on comment form:
    - Add flash message styles
    - Hide form after submit
    - Catch nocaptcha validation errors and prompt user to fix them
    - Sorry for all the bad indentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ date_format_library: jquery-dateFormat.min.js
 {% include_relative _lib/{{page.date_format_library}} %}
 
 $(document).ready(function() {
+    
   var menuToggle = $('#js-mobile-menu').unbind();
   $('#js-navigation-menu').removeClass("show");
 
@@ -41,8 +42,6 @@ $(document).ready(function() {
 
         // Assign JSON data points to variables.
         var name = json.data[i].name;
-        // TODO: fix this
-        // var created = $.format.date(json.data[i].created_at, "MMM d, yyyy h:mma");
         var created = json.data[i].created_at;
         var comment = json.data[i].comment;
 
@@ -63,44 +62,53 @@ $(document).ready(function() {
       // Prevent default action.
       e.preventDefault();
 
-        var data = form.serialize();
-
         // Send ajax request.
         $.ajax({
           url: commentServer+'/api/comments/new',
           type: 'POST',
           dataType: 'json',
-          data: data,
+          data: form.serialize(),
           beforeSend: function(){
             // Change submit button value text and disable it.
-            submit.val('Submitting...').attr('disabled', 'disabled');
+              // submit.val('Submitting...').attr('disabled', 'disabled');
+              $('.flash-error').remove();
+              $('.flash-success').remove();
           },
-          success: function(data){
+            success: function(data){
+                // Check to see if we got an error from the server.
+                if (data.success == false) {
+                    var message = '<div class="flash-error">' + data.message + '</div>';
+                    $('#post-comments').prepend(message);
+                    return;
+                }
+
             var name = $(data)[0].data[0].name;
-            // // TODO: fix this
+
             // // var created = $.format.date(json.data[i].created_at, "MMM d, yyyy h:mma");
             var created = $(data)[0].data[0].created_at;
             console.log(data);
             var comment = $(data)[0].data[0].comment;
 
-            // // Create HTML output.
-            var outhtml = '';
-            outhtml = outhtml + '<div class="comment"><h5>'+name+' says:</h5>';
+            // Create HTML output.
+              var thanks = '<div class="flash-success">Thanks for submitting your comment!</div>';
+                $('#post-comments').prepend(thanks);
+                form.trigger('reset');
+                form.hide();
+
+                // Append new comment.
+              var outhtml = '';
+            outhtml = outhtml + '<div class="comment new"><h5>'+name+' says:</h5>';
             outhtml = outhtml + '<p class="comment-date">'+created+'</p>';
             outhtml = outhtml + '<p class="comment-text">'+comment+'</p></div>';
 
               // Append with fadeIn, see http://stackoverflow.com/a/978731
               var item = $(outhtml).hide().fadeIn(800);
-              $('#post-comments').append(item);
-              console.log(item);
-              // Reset form and button.
-              // @todo Hide the form entirely.
-              // @todo Check data.success to make sure it's true, otherwise display error.
-            },
+                $('#post-comments').append(item);
+           },
             error: function(e){
               form.trigger('reset');
               submit.val('Submit').removeAttr('disabled');
-              $('#post-comments').append('<div class="comment error">' + 'An error occurred. Sorry ¯\\\_(ツ)_/¯' + '</div>');
+              $('#post-comments').prepend('<div class="flash-error">' + 'An error occurred. Sorry ¯\\\_(ツ)_/¯' + '</div>');
             }
         });
     });
