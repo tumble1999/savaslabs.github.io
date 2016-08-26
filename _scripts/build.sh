eb2e9c69af8ea55186a33d4e441e0c323556ed8e
Anne Tomasevich <annetvich@gmail.com>
2016-08-26 14:19:41 -0400

    Issue #2084: Delete contents of master before adding _site

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -8,10 +8,11 @@ pwd
 git status
 ls -lah
 git branch
+git remote -v
 
 # Clone master branch using encrypted GH_TOKEN for authentication.
 git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
-git status
+rm -rf ../savaslabs.github.io.master/
 ls -lah ../savaslabs.github.io.master
 
 # Copy generated HTML site to master branch.
@@ -21,9 +22,9 @@ ls -lah ../savaslabs.github.io.master
 
 # Commit and push generated content to master branch.
 cd ../savaslabs.github.io.master
-ls -lah ../savaslabs.github.io.master
 git config user.email ${GH_EMAIL}
 git config user.name "savas-bot"
 git add -A .
+git status
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --quiet origin master > /dev/null 2>&1
