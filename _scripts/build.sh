e4df8afa4809ad10bcbdc651fd97c440b9deebea
Anne Tomasevich <annetvich@gmail.com>
2016-08-26 13:29:32 -0400

    Issue #2084: Add debug code to build script

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,16 +4,29 @@
 set -e
 
 # Clean up master branch.
+git status
+ls -lah
+ls -lah ../savaslabs.github.io.master
 rm -rf ../savaslabs.github.io.master
+git status
+ls -lah
+ls -lah ../savaslabs.github.io.master
 
 # Clone master branch using encrypted GH_TOKEN for authentication.
 git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
+git status
+ls -lah
+ls -lah ../savaslabs.github.io.master
 
 # Copy generated HTML site to master branch.
 cp -R _site/* ../savaslabs.github.io.master
+git status
+ls -lah
+ls -lah ../savaslabs.github.io.master
 
 # Commit and push generated content to master branch.
 cd ../savaslabs.github.io.master
+ls -lah
 git config user.email ${GH_EMAIL}
 git config user.name "savas-bot"
 git add -A .
