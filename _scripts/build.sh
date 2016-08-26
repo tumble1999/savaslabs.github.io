db7a0d7a6d48eb9a0a138841fb1175f85e240308
Anne Tomasevich <annetvich@gmail.com>
2016-08-26 13:48:35 -0400

    Issue #2084: More debugging code

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,23 +4,26 @@
 set -e
 
 # Clean up master branch.
+pwd
+git status
+ls -lah
+ls -lah ../savaslabs.github.io.master
+git branch
 rm -rf ../savaslabs.github.io.master
 
 # Clone master branch using encrypted GH_TOKEN for authentication.
 git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
 git status
-ls -lah
 ls -lah ../savaslabs.github.io.master
 
 # Copy generated HTML site to master branch.
 cp -R _site/* ../savaslabs.github.io.master
 git status
-ls -lah
 ls -lah ../savaslabs.github.io.master
 
 # Commit and push generated content to master branch.
 cd ../savaslabs.github.io.master
-ls -lah
+ls -lah ../savaslabs.github.io.master
 git config user.email ${GH_EMAIL}
 git config user.name "savas-bot"
 git add -A .
