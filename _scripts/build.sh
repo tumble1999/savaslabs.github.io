3b5186f09e4cc16e55fb437ab63374fbf74c3ba8
Anne Tomasevich <annetvich@gmail.com>
2016-08-26 14:47:43 -0400

    Issue #2084: Switch to master branch before committing

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,20 +3,29 @@
 # Enable error reporting to the console.
 set -e
 
-# Clone master branch using encrypted GH_TOKEN for authentication.
+# Clone master branch into a new directory using encrypted GH_TOKEN for
+# authentication.
 git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
-rm -rf ../savaslabs.github.io.master/*
-ls -lah ../savaslabs.github.io.master
 
-# Copy generated HTML site to master branch.
-cp -R _site/* ../savaslabs.github.io.master
-ls -lah ../savaslabs.github.io.master
+# Move to new directory and copy generated HTML site from source.
+cd ../savaslabs.github.io.master
+ls -lah
+rm -rf *
+ls -lah
+cp -R ../savaslabs.github.io/_site/* .
+ls -lah
+
+# Switch to the master branch, keeping the copied _site directory.
+git stash
+git status
+git checkout master
+git status
+git stash apply
+git status
 
 # Commit and push generated content to master branch.
-cd ../savaslabs.github.io.master
 git config user.email ${GH_EMAIL}
 git config user.name "savas-bot"
-git status
 git add -A .
 git status
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
