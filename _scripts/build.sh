7e6dbab4116f967717a6f88967b29bceb741bf33
Anne Tomasevich <annetvich@gmail.com>
2016-08-26 14:23:04 -0400

    Issue #2084: Don't actually remove director, just contents

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -12,7 +12,7 @@ git remote -v
 
 # Clone master branch using encrypted GH_TOKEN for authentication.
 git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
-rm -rf ../savaslabs.github.io.master/
+rm -rf ../savaslabs.github.io.master/*
 ls -lah ../savaslabs.github.io.master
 
 # Copy generated HTML site to master branch.
