e88aeab93b37a1a52f5e4ac046a7d1a80ed4f16a
Anne Tomasevich <annetvich@gmail.com>
2016-08-26 15:25:19 -0400

    Issue #2084: Remove debug code and clarify comments

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,11 +3,8 @@
 # Enable error reporting to the console.
 set -e
 
-# Need to see if this exists.
-ls -lah ../savaslabs.github.io.master
-
-# Clone master branch into a new directory using encrypted GH_TOKEN for
-# authentication.
+# Clone repo (defaulted to source branch) into a new directory using encrypted
+# GH_TOKEN for authentication.
 git clone https://${GH_TOKEN}@github.com/savaslabs/savaslabs.github.io.git ../savaslabs.github.io.master
 
 # Check out master branch on new repo and remove everything.
@@ -17,7 +14,7 @@ git config user.name "savas-bot"
 git checkout master
 rm -rf *
 
-# Move to new directory and copy generated HTML site from source.
+# Copy generated HTML site from source branch in original repo.
 # Now the master branch will contain only the contents of the _site directory.
 cp -R ../savaslabs.github.io/_site/* .
 
